fc111528b09a4a07f9cdb21ab194dc06f64abc22
pjmantoss <joelptoss@gmail.com>
2020-11-07 18:00:54 +0100

    update script

--- script.js
+++ script.js
@@ -2,6 +2,19 @@ const  getPosts = async => {
     const postsPromise = await fetch("");
 
     const posts = await postsPromise.json();
+    let html = "";
+
+    posts.slice(0, 5).forEach(post => {
+        const title = post.title;
+        const body = post.body;
+
+        html += `
+            <div class="post">
+                <h3>${title}</h3>
+                <p>${body}</p>
+            </div>
+        `
+    })
 };
 
 getPosts();
